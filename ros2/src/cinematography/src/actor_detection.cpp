956419a08e58861ef3fe8eb68a90147aaf3c887c
oren <virtualoren@gmail.com>
2020-10-20 23:13:01 -0500

    Fixed gimbal in actor_detection. Misc cleanup

--- ros2/src/cinematography/src/actor_detection.cpp
+++ ros2/src/cinematography/src/actor_detection.cpp
@@ -10,6 +10,7 @@
 #include "builtin_interfaces/msg/time.hpp"
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include "vehicles/multirotor/api/MultirotorRpcLibClient.hpp"
 //#include <darknet/include/darknet.h>
 #include <math.h>
 
@@ -20,8 +21,9 @@ private:
     rclcpp::Publisher<cinematography_msgs::msg::BoundingBox>::SharedPtr bb_pub;
     rclcpp::Publisher<cinematography_msgs::msg::GimbalAngleQuatCmd>::SharedPtr gimbal_control;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera;
+    msr::airlib::MultirotorRpcLibClient airsim_client;
     tf2::Quaternion gimbal_setpoint;
-    std::string camera_name = "front_center_custom";
+    std::string camera_name = "front_left_custom";  // TODO: Set these as parameters
     std::string vehicle_name = "drone_1";
     float fov = M_PI/2;
     int gimbal_msg_count = 0;
@@ -51,32 +53,27 @@ private:
         float height = array[3] / 1024.0;
 
         //DEBUGGING
-        centerx = 0.4;
-        centery = 0.501;
+        centerx = 0.45;
+        centery = 0.505;
         width=0.15;
         height=0.2;
 
         // Use centering coordinates to move gimbal so actor is in center frame (OPTIONAL: Add parameter to specify
         // where in frame the actor should be)
         tf2::Quaternion horiz_adjustment = tf2::Quaternion(0, 0, sin(fov * (centerx - 0.5) / 2), cos(fov * (centerx - 0.5) / 2));
-        tf2::Quaternion vert_adjustment = tf2::Quaternion(sin(fov * centery - height / 2), 0, 0, cos(fov * centery - height / 2));
+        tf2::Quaternion vert_adjustment = tf2::Quaternion(0, sin(fov * (centery + height/2 - 0.5) / 2), 0, cos(fov * (centery + height/2 - 0.5) / 2));
         tf2::Quaternion actor_direction = horiz_adjustment * gimbal_setpoint * vert_adjustment;   // Get the orientation in the direction of the actor (centered on bottom center)
 
-        vert_adjustment = tf2::Quaternion(sin(fov * (centery - 0.5) / 2), 0, 0, cos(fov * (centery - 0.5) / 2));
+        vert_adjustment = tf2::Quaternion(0, sin(fov * (centery - 0.5) / 2), 0, cos(fov * (centery - 0.5) / 2));
         gimbal_setpoint = horiz_adjustment * gimbal_setpoint * vert_adjustment;             // Get new gimbal setpoint (centered on actor)
 
-        cinematography_msgs::msg::GimbalAngleQuatCmd gb_msg;
-        gb_msg.header.frame_id = "world_ned";
-        gb_msg.header.stamp = this->now();
-        gb_msg.camera_name = camera_name;
-        gb_msg.vehicle_name = vehicle_name;
-        gb_msg.orientation = tf2::toMsg(actor_direction);
-
-        gimbal_control->publish(gb_msg);
+        msr::airlib::Quaternionr alq = msr::airlib::Quaternionr(gimbal_setpoint.w(),
+                        gimbal_setpoint.x(), gimbal_setpoint.y(), gimbal_setpoint.z());
+        airsim_client.simSetCameraOrientation(camera_name, alq, vehicle_name);
 
         // Extract subimage, package with centering coordinates, and publish to /bounding_box
         cinematography_msgs::msg::BoundingBox bb;
-        bb.actor_direction = tf2::toMsg(gimbal_setpoint);
+        bb.actor_direction = tf2::toMsg(actor_direction);
 
         int px_left = (centerx - width/2) * msg->width;
         int px_top = (centery - height/2) * msg->height;
@@ -91,7 +88,9 @@ private:
     }
 
 public:
-    ActorDetection() : Node("actor_detection") {
+    // TODO: Pass IP address of airsim as parameter
+    ActorDetection() : Node("actor_detection"), airsim_client("localhost") {
+        gimbal_setpoint = tf2::Quaternion(0,0,0,1);
         bb_pub = this->create_publisher<cinematography_msgs::msg::BoundingBox>("bounding_box", 50);
         gimbal_control = this->create_publisher<cinematography_msgs::msg::GimbalAngleQuatCmd>("gimbal", 1);
         camera = this->create_subscription<sensor_msgs::msg::Image>("camera", 50, std::bind(&ActorDetection::processImage, this, _1));
