ff35abadeaeff48cc8d869988ca970f95b2c3ab9
oren <virtualoren@gmail.com>
2021-01-05 22:47:35 -0600

    Added launcher for debug viz

---
+++ ros2/src/cinematography/launch/debug_rviz.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="rviz2",
+            node_executable="rviz2",
+            node_name="rviz2"
+        ),
+        Node(
+            package="rqt_image_view",
+            node_executable="rqt_image_view",
+            node_name="rqt_image_view",
+            parameters=[
+                {"image" : "/airsim_ros2_wrapper/camera"}
+            ]
+        ),
+        Node(
+            package="cinematography",
+            node_executable="debug_viz",
+            node_name="debug_viz",
+            output="screen",
+            remappings=[
+                ("pose_out", "/auto_cinematography/debug/pose"),
+                ("traj_out", "/auto_cinematography/debug/actor_traj"),
+                ("img_out", "/auto_cinematography/debug/camera"),
+                ("pose_in", "/airsim_ros2_wrapper/pose/drone_1/front_center_custom"),
+                ("traj_in", "/auto_cinematography/planning/actor_traj"),
+                ("img_in", "/airsim_ros2_wrapper/camera"),
+                ("vm_in", "/auto_cinematography/vision/vision_measurements")
+            ],
+        )
+    ])
---
+++ ros2/src/cinematography/src/debug_viz.cpp
@@ -0,0 +1,136 @@
+#include "rclcpp/rclcpp.hpp"
+#include <boost/make_shared.hpp>
+#include "builtin_interfaces/msg/time.hpp"
+#include "cinematography_msgs/msg/multi_do_farray.hpp"
+#include "cinematography_msgs/msg/vision_measurements.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class DebugViz : public rclcpp::Node {
+private:
+    rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr pose_sub;
+    rclcpp::Subscription<cinematography_msgs::msg::MultiDOFarray>::SharedPtr traj_sub;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr img_sub;
+    rclcpp::Subscription<cinematography_msgs::msg::VisionMeasurements>::SharedPtr vm_sub;
+
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub;
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr traj_pub;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub;
+
+    std::recursive_mutex m;
+    rclcpp::Clock clock = rclcpp::Clock(RCL_SYSTEM_TIME);
+    std::string world_frame_id_ = "world_ned";
+
+    cv::Mat lastFrame;
+
+    void fetchDronePose(const geometry_msgs::msg::Pose::SharedPtr pose) {
+        geometry_msgs::msg::PoseStamped ps;
+
+        ps.pose = *pose;
+        ps.header.frame_id = world_frame_id_;
+        ps.header.stamp = clock.now();
+
+        pose_pub->publish(ps);
+    }
+
+    void fetchActorTraj(const cinematography_msgs::msg::MultiDOFarray::SharedPtr traj) {
+        geometry_msgs::msg::PoseArray traj_out;
+
+        traj_out.header.frame_id = "world_ned";
+        traj_out.header.stamp = this->now();
+
+        traj_out.poses.reserve(traj->points.size());
+
+        for(cinematography_msgs::msg::MultiDOF md : traj->points) {
+            geometry_msgs::msg::Pose p;
+            p.position.x = md.x;
+            p.position.y = md.y;
+            p.position.z = md.z;
+            p.orientation.w = cos(md.yaw/2);
+            p.orientation.x = 0;
+            p.orientation.y = 0;
+            p.orientation.z = sin(md.yaw/2);
+
+            traj_out.poses.push_back(p);
+        }
+
+        traj_pub->publish(traj_out);
+    }
+
+    void fetchImage(const sensor_msgs::msg::Image::SharedPtr img) {
+        m.lock();
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, img->encoding);
+        lastFrame = cv_ptr->image;
+        m.unlock();
+    }
+
+    void fetchVisionMeasurements(const cinematography_msgs::msg::VisionMeasurements::SharedPtr vm) {
+        int baseline = 0;
+        float font_scale = 0.5;
+        int thickness = 2;
+
+        cv::Point topLeftCorner = cv::Point((vm->centerx - vm->width/2) * lastFrame.cols,
+                                            (vm->centery - vm->height/2) * lastFrame.rows);
+        cv::Point bottomRightCorner = cv::Point((vm->centerx + vm->width/2) * lastFrame.cols,
+                                                (vm->centery + vm->height/2) * lastFrame.rows);
+
+        int length = lastFrame.cols / 10;
+        cv::Point center = cv::Point(vm->centerx * lastFrame.cols, vm->centery * lastFrame.rows);
+        cv::Point endpoint = cv::Point(vm->centerx * lastFrame.cols + length * sin(vm->hde),
+                                       vm->centery * lastFrame.rows + length * cos(vm->hde));
+
+
+        cv_bridge::CvImage cv_msg;
+        cv_msg.header.frame_id = "world_ned";
+        cv_msg.header.stamp = this->now();
+        cv_msg.encoding = sensor_msgs::image_encodings::BGR8;
+
+        m.lock();
+
+        if (vm->width != 0) {
+            // draw rectangle
+            cv::rectangle(lastFrame, topLeftCorner, bottomRightCorner, cv::Scalar(50, 205, 50), 2); 
+
+            //draw hde line
+            cv::line(lastFrame, center, endpoint, cv::Scalar(0,0,255), 3);
+        }
+
+        cv_msg.image = lastFrame;
+        img_pub->publish(cv_msg.toImageMsg());
+        m.unlock();
+    }
+    
+
+public:
+    DebugViz() : Node("debug_viz") {
+        pose_pub = this->create_publisher<geometry_msgs::msg::PoseStamped>("pose_out", 1);
+        traj_pub = this->create_publisher<geometry_msgs::msg::PoseArray>("traj_out", 1);
+        img_pub = this->create_publisher<sensor_msgs::msg::Image>("img_out", 20);
+        
+        pose_sub = this->create_subscription<geometry_msgs::msg::Pose>("pose_in", 1,
+            std::bind(&DebugViz::fetchDronePose, this, _1));
+        traj_sub = this->create_subscription<cinematography_msgs::msg::MultiDOFarray>("traj_in", 1,
+            std::bind(&DebugViz::fetchActorTraj, this, _1));
+        img_sub = this->create_subscription<sensor_msgs::msg::Image>("img_in", 1,
+            std::bind(&DebugViz::fetchImage, this, _1));
+        vm_sub = this->create_subscription<cinematography_msgs::msg::VisionMeasurements>("vm_in", 1,
+            std::bind(&DebugViz::fetchVisionMeasurements, this, _1));
+    }
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::executors::MultiThreadedExecutor exec;
+    auto ros2wrapper = std::make_shared<DebugViz>();
+    exec.add_node(ros2wrapper);
+    exec.spin();
+    rclcpp::shutdown();
+
+    return 0;
+}
