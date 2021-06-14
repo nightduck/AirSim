722100b22652e91f6f4b71907e33f30789bfa1a1
Oren Bell <oren.bell@wustl.edu>
2021-06-14 17:30:41 -0500

    Added yolo4tiny to trt build script

--- build_trt_engines.sh
+++ build_trt_engines.sh
@@ -12,12 +12,22 @@ mkdir -p ros2/src/cinematography/tkDNN/build/yolo4_deer
 cp -r yolo4_deer/debug ros2/src/cinematography/tkDNN/build/yolo4_deer/
 cp -r yolo4_deer/layers ros2/src/cinematography/tkDNN/build/yolo4_deer/
 
-# Build obj det trt engine
+# Extract yolo4 tiny obj detection model
+cp yolo4tiny_deer/yolo4tiny_deer.cpp ros2/src/cinematography/tkDNN/tests/darknet/
+cp yolo4tiny_deer/yolo4tiny_deer.cfg ros2/src/cinematography/tkDNN/tests/darknet/cfg/
+cp yolo4tiny_deer/yolo4tiny_deer.names ros2/src/cinematography/tkDNN/tests/darknet/names/
+mkdir -p ros2/src/cinematography/tkDNN/build/yolo4tiny_deer
+cp -r yolo4tiny_deer/debug ros2/src/cinematography/tkDNN/build/yolo4tiny_deer/
+cp -r yolo4tiny_deer/layers ros2/src/cinematography/tkDNN/build/yolo4tiny_deer/
+
+# Build yolo4 tiny trt engines
 cd ros2/src/cinematography/tkDNN/build
 cmake ..
 make -j $(nproc)
 ./test_yolo4_deer
+./test_yolo4tiny_deer yolo4tiny_deer ../tests/darknet/cfg/yolo4tiny_deer.cfg ../tests/darknet/names/yolo4tiny_deer.names
 cp yolo4_deer_fp32.rt ../../../../../
+cp yolo4tiny_deer_fp32.rt ../../../../../
 cd ../../../../../
 
 # Build hde trt engine
@@ -28,4 +38,4 @@ cp deer_hde_fp32.rt ../
 
 # Cleanup
 cd ../
-rm -r yolo4_deer trt_builder models_weights.zip
+rm -r yolo4_deer yolo4tiny_deer trt_builder models_weights.zip
